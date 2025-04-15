9783d5eb0f09fc82e21f41ae7aadb4360df67392
hxgoantonio <hxgonzalezc@gmail.com>
2025-04-15 10:04:27 -0400

    Archivos añadidos

---
+++ trn.py
@@ -0,0 +1,21 @@
+#---------------------------------------------------------------------
+# Variable selection and coefficient estimation
+#---------------------------------------------------------------------
+
+import numpy as np
+import pandas as pd
+    
+#---------------------------------------------------------------------
+# Variable selection by use co-linear index
+def selection_vars():
+    while True:
+        ...       
+    return( )
+#---------------------------------------------------------------------
+def main():            
+    X = load_data_csv('dataset.csv')	   
+    
+
+if __name__ == '__main__':   
+	 main()
+# -----------------------------
